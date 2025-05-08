a2b88e40a4695daf52b7439ff68e9fba51000abf
Gijs van Male <g.van.male@student.rug.nl>
2025-05-08 15:17:06 +0200

    importing data from Soarremoarre

--- Scripts/1. Data_import.R
+++ Scripts/1. Data_import.R
@@ -22,8 +22,14 @@ str(deployment_RM23)
 observations_RM23 <- read_csv ("https://docs.google.com/spreadsheets/d/e/2PACX-1vRZdYiSTRKrAdmdOUnb-r_tscKRueVX2cUdxvu3PShekqh5mZd2wl5EetNs0a4IrQ/pub?gid=1367235605&single=true&output=csv")
 str(observations_RM23)
 
+# Soarremoarre data 2023
+deployment_SM23 <- read_csv ("https://docs.google.com/spreadsheets/d/e/2PACX-1vS6uwGxVTcQZ2IzUkw7V59lQgWpCEqwYdQNHW7_6vLIofVs6ZAfRUJyc1tcInd4Cw/pub?gid=1033951230&single=true&output=csv")
+str(deployment_SM23)
+
+observations_SM23 <- read_csv ("https://docs.google.com/spreadsheets/d/e/2PACX-1vQmSi1yTINru3K_xom415DDZ8P-Su8Gm69DV6Kdtz0QrrJuBLt6X9_CX_SQ2vuV9A/pub?gid=799909957&single=true&output=csv")
+str(observations_SM23)
 # Structure data ----
-# Structuring the observations data
+# Structuring the observations data RM----
 observations_RM23_filtered <- observations_RM23 |>
   dplyr:: select(-c(
     "mediaID",
@@ -96,26 +102,84 @@ observations_RM23_filtered <- observations_RM23_filtered |>
     deployment_RM23_filtered,
     by = "deploymentID" )
 
-# Short overview of the data ----
-# Zuid-West Friesland
-SW_summary <- SW_data |>
+
+# Structuring the observations data SM----
+observations_SM23_filtered <- observations_SM23 |>
+  dplyr:: select(-c(
+    "mediaID",
+    "cameraSetupType",
+    "lifeStage",
+    "sex",
+    "behavior",
+    "individualID",
+    "individualPositionRadius",
+    "individualPositionAngle",
+    "individualSpeed",
+    "bboxX",
+    "bboxY",
+    "bboxWidth",
+    "bboxHeight",
+    "observationTags",
+    "observationComments",
+    "classificationProbability",
+    "classificationTimestamp",
+    "observationLevel",
+    "observationType")
+  ) |>
+  dplyr::mutate(
+    study_year = format(eventStart, "%Y"),
+    hour = as.numeric(format(eventStart, "%H")),
+    minute = as.numeric(format(eventStart, "%M")),
+    study_date = format(eventStart, "%m/%d/%Y"),
+  ) |>
+  dplyr::filter(study_year == "2023")
+
+# Structuring the deployment data
+deployment_SM23_filtered <- deployment_SM23 |>
+  dplyr::select(-c(
+    "coordinateUncertainty",
+    "setupBy",
+    "cameraID",
+    "cameraModel",
+    "cameraDelay",
+    "cameraHeight",
+    "cameraDepth",
+    "cameraTilt",
+    "cameraHeading",
+    "detectionDistance",
+    "timestampIssues",
+    "baitUse",
+    "habitat",
+    "deploymentGroups",
+    "deploymentTags"
+  )) |>
+  dplyr::mutate(
+    latitude = as.numeric(str_replace(as.character(latitude), "^(.{2})(.+)$", "\\1.\\2")),
+    longitude = as.numeric(str_replace(as.character(longitude), "^(.{1})(.+)$", "\\1.\\2")),
+    deploymentStart = lubridate::ymd_hms(deploymentStart) 
+  ) |>
+  dplyr::mutate(
+    study_year = format(deploymentStart, "%Y"),
+    hour = format(deploymentStart, "%H"),
+    minute = format(deploymentStart, "%M"),
+    study_date = format(deploymentStart, "%m/%d/%Y")
+  ) |>
   dplyr::filter(study_year == "2023") |>
   dplyr::select(
     "deploymentID",
     "locationName",
-    "scientificName",
-    "eventStart",
-    "eventEnd",
-  ) |>
-  dplyr::group_by(scientificName) |>
-  dplyr::summarise(
-    n = n(),
-    .groups = 'drop'
-  )|>
-  dplyr::arrange(desc(n))
-SW_summary
+    "latitude",
+    "longitude"
+  ) # Warning occurs because 82 entries have an invalid data entry
 
-# Reitdiep Midden
+
+observations_SM23_filtered <- observations_SM23_filtered |>
+  dplyr::left_join(
+    deployment_SM23_filtered,
+    by = "deploymentID" )
+
+# Short overview of the data ----
+
 # Species of interest
 RM_species_of_interest <- c(
   "Vulpes vulpes",
@@ -139,6 +203,25 @@ RM_species_of_interest <- c(
   "Rattus norvegicus"
 )
 
+# Zuid-West Friesland
+SW_summary <- SW_data |>
+  dplyr::filter(study_year == "2023") |>
+  dplyr::select(
+    "deploymentID",
+    "locationName",
+    "scientificName",
+    "eventStart",
+    "eventEnd",
+  ) |>
+  dplyr::group_by(scientificName) |>
+  dplyr::summarise(
+    n = n(),
+    .groups = 'drop'
+  )|>
+  dplyr::arrange(desc(n))
+SW_summary
+
+# Reitdiep Midden
 RM_summary <- observations_RM23_filtered |>
   dplyr::filter(scientificName %in% RM_species_of_interest) |>
   dplyr::select(
@@ -155,3 +238,21 @@ RM_summary <- observations_RM23_filtered |>
   )|>
   dplyr::arrange(desc(n))
 RM_summary
+
+# Soarremoarre
+SM_summary <- observations_SM23_filtered |>
+  dplyr::filter(scientificName %in% RM_species_of_interest) |>
+  dplyr::select(
+    "deploymentID",
+    "locationName",
+    "scientificName",
+    "eventStart",
+    "eventEnd",
+  ) |>
+  dplyr::group_by(scientificName) |>
+  dplyr::summarise(
+    n = n(),
+    .groups = 'drop'
+  )|>
+  dplyr::arrange(desc(n))
+SM_summary
