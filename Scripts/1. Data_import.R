56ece59929bd9e803503915ee4ab4b99f09a5f6a
Gijs van Male <g.van.male@student.rug.nl>
2025-05-14 12:07:53 +0200

    working on the down time of the camera's

--- Scripts/1. Data_import.R
+++ Scripts/1. Data_import.R
@@ -27,6 +27,9 @@ observations_RM23 <- read_csv ("https://docs.google.com/spreadsheets/d/e/2PACX-1
                 eventEnd = with_tz(ymd_hms(eventEnd), tzone = "Europe/Amsterdam"))
 str(observations_RM23)
 
+RM_down_data <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSsKxAXP2OhDfWElQAYGlkEmKDFJg596Oyuc3hwth-KaQlcNTZWebvJJHmcJ6p9mA/pub?gid=375727651&single=true&output=csv")
+str(RM_down_data)
+
 # Soarremoarre data 2023
 deployment_SM23 <- read_csv ("https://docs.google.com/spreadsheets/d/e/2PACX-1vS6uwGxVTcQZ2IzUkw7V59lQgWpCEqwYdQNHW7_6vLIofVs6ZAfRUJyc1tcInd4Cw/pub?gid=1033951230&single=true&output=csv")
 str(deployment_SM23)
@@ -35,6 +38,25 @@ observations_SM23 <- read_csv ("https://docs.google.com/spreadsheets/d/e/2PACX-1
   dplyr::mutate(eventStart = with_tz(ymd_hms(eventStart), tzone = "Europe/Amsterdam"),
                 eventEnd = with_tz(ymd_hms(eventEnd), tzone = "Europe/Amsterdam"))
 str(observations_SM23)
+
+SM_down_data <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vT25ocRok1-g2wjBhBg3DGGTmIYYYvHW9KvQoxOgLNLM84PYCrBMWpv0S0nzMo4xA/pub?gid=2002383610&single=true&output=csv") |>
+  dplyr:: mutate(
+    start_downtime = mdy(start_downtime),
+    end_downtime = mdy(end_downtime),
+    total_down_days = as.integer(end_downtime - start_downtime) + 1
+  )
+str(SM_down_data)
+
+Analysis_species <-c(
+  "Vulpes vulpes",
+  "Martes",
+  "Felis",
+  "Felis catus",
+  "Mustela putorius",
+  "Mustela erminea",
+  "Martes foina"
+)
+
 # Structure data ----
 # Structuring the observations data RM----
 observations_RM23_filtered <- observations_RM23 |>
@@ -311,3 +333,26 @@ Combi_summary <- combined_data |>
   )|>
   dplyr::arrange(desc(n))
 Combi_summary
+
+# Sum data per species per location 2023----
+# Sum data per species per location 2023 SM ----
+SM_down_summary <- SM_down_data |>
+  dplyr::group_by(locationName) |>
+  dplyr::summarise(total_down_days = sum(total_down_days), .groups = "drop")
+
+SM_location <- observations_SM23_filtered |>
+  dplyr::filter(!is.na(scientificName),
+                scientificName %in% Analysis_species) |>
+  dplyr::  mutate(
+    scientificName = case_when(
+      scientificName == "Felis" ~ "Felis catus",
+      scientificName == "Martes" ~ "Martes foina",
+      TRUE ~ scientificName
+    )) |>
+  dplyr::distinct(scientificName, locationName, study_date) |>
+  dplyr::group_by(locationName, scientificName) |>
+  dplyr::summarise(days_detected = n(), .groups = "drop") |>
+  pivot_wider(names_from = scientificName, values_from = days_detected, values_fill = 0)
+
+SM_location <- SM_location |>
+  left_join(SM_down_summary, by = "locationName")
