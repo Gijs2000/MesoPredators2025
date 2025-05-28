075bd8035f04928f98b1a0c078ad629229faff97
Gijs van Male <g.van.male@student.rug.nl>
2025-05-28 15:47:20 +0200

    calculating all time differences between species

--- Scripts/7. Time_to_event_analysis.R
+++ Scripts/7. Time_to_event_analysis.R
@@ -2,3 +2,60 @@
 #' Start date: 28 may 2025
 #' Author: Gijs van Male
 #' Supervisors: Chris Smit, Rienk Fokkema, Pieter Otte
+
+
+################### REITDIEPMIDDEN ###################
+# Setting up the dataset correctly for RM----
+RM_time2event_data <- observations_RM23_filtered |>
+  dplyr::filter(scientificName %in% Analysis_species) |>
+  dplyr::mutate(scientificName =case_when(
+    scientificName == "Felis" ~ "Felis catus",
+    scientificName == "Martes" ~ "Martes foina",
+    TRUE ~ scientificName
+  )) |>
+  dplyr::arrange(locationName, eventStart) |>
+  dplyr::group_by(locationName) |>
+  dplyr::mutate(
+    next_species = lead(scientificName),
+    timegap = as.numeric(difftime(lead(eventStart), eventStart, units = "mins"))) |>
+  dplyr::ungroup() |>
+  dplyr::select(locationName, scientificName, next_species, timegap) |>
+  dplyr::filter(scientificName != next_species)
+
+################### SOARREMOARRE ###################
+# Setting up the dataset correctly for SM----
+SM_time2event_data <- observations_SM23_filtered |>
+  dplyr::filter(scientificName %in% Analysis_species) |>
+  dplyr::mutate(scientificName =case_when(
+    scientificName == "Felis" ~ "Felis catus",
+    scientificName == "Martes" ~ "Martes foina",
+    TRUE ~ scientificName
+  )) |>
+  dplyr::arrange(locationName, eventStart) |>
+  dplyr::group_by(locationName) |>
+  dplyr::mutate(
+    next_species = lead(scientificName),
+    timegap = as.numeric(difftime(lead(eventStart), eventStart, units = "mins"))) |>
+  dplyr::ungroup() |>
+  dplyr::select(locationName, scientificName, next_species, timegap) |>
+  dplyr::filter(scientificName != next_species)
+
+################### SOUTHWEST FRIESLAND ###################
+# Setting up the dataset correctly for SM----
+SW_time2event_data <- SW_data |>
+  dplyr::filter(scientificName %in% Analysis_species) |>
+  dplyr::mutate(scientificName =case_when(
+    scientificName == "Felis" ~ "Felis catus",
+    scientificName == "Martes" ~ "Martes foina",
+    TRUE ~ scientificName
+  )) |>
+  dplyr::arrange(locationName, eventStart) |>
+  dplyr::group_by(locationName) |>
+  dplyr::mutate(
+    next_species = lead(scientificName),
+    timegap = as.numeric(difftime(lead(eventStart), eventStart, units = "mins"))) |>
+  dplyr::ungroup() |>
+  dplyr::select(locationName, scientificName, next_species, timegap) |>
+  dplyr::filter(scientificName != next_species)
+
+
