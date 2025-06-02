74d4983569abdae683a07a7f486be1522fb1ceed
Gijs van Male <g.van.male@student.rug.nl>
2025-06-02 16:41:17 +0200

    playing aroung with graphs

--- Scripts/7. Time_to_event_analysis.R
+++ Scripts/7. Time_to_event_analysis.R
@@ -3,6 +3,12 @@
 #' Author: Gijs van Male
 #' Supervisors: Chris Smit, Rienk Fokkema, Pieter Otte
 
+# Load packages----
+{
+  library(tidyverse)
+  library(survival)
+  library(survminer)
+  }
 
 ################### REITDIEPMIDDEN ###################
 # Setting up the dataset correctly for RM----
@@ -20,8 +26,59 @@ RM_time2event_data <- observations_RM23_filtered |>
     timegap = as.numeric(difftime(lead(eventStart), eventStart, units = "mins"))) |>
   dplyr::ungroup() |>
   dplyr::select(locationName, scientificName, next_species, timegap) |>
-  dplyr::filter(scientificName != next_species)
+  dplyr::filter(scientificName != next_species)|>
+  dplyr::mutate(event = 1) |>
+  mutate(timegap_hours = timegap / 60) |>
+  na.omit()
 
+#Plots RM----
+RM_time2event_data_summary <- RM_time2event_data %>%
+  group_by(focal_species = scientificName, recent_species = next_species) %>%
+  summarise(
+    mean_gap_hours = mean(timegap) ,          
+    sd = sd(timegap),
+    n = n(),
+    se = sd / sqrt(n),
+    lower_CI = mean_gap_hours - 1.96 * se,
+    upper_CI = mean_gap_hours + 1.96 * se,
+    .groups = "drop"
+  )
+
+ggplot(RM_time2event_data_summary, aes(x = recent_species, y = mean_gap_hours, color = recent_species)) +
+  geom_point(size = 3) +
+  geom_errorbar(aes(ymin = lower_CI, ymax = upper_CI), width = 0.2) +
+  facet_wrap(~ focal_species, scales = "free_y") +
+  labs(
+    title = "Effect of recent species detection on wait time Reitdiep",
+    x = "Previous detected species",
+    y = "Mean no. minutes between detections"
+  ) +
+  geom_text(aes(label = n), vjust = -1.5, size = 3.5, color = "black")+
+  theme_minimal(base_size = 14) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    legend.position = "none",
+    strip.background = element_rect(fill = "#CCCCCC"),
+    strip.text = element_text(face = "bold")
+  )
+
+RM_surv <- Surv(time = RM_time2event_data$timegap_hours, event = RM_time2event_data$event)
+
+RM_km_fit <- survfit(RM_surv ~ scientificName, data = RM_time2event_data)
+
+ggsurvplot(
+  RM_km_fit,
+  data = RM_time2event_data,
+  conf.int = TRUE,
+  surv.scale = "percent",
+  xlab = "Time since species detected (hours)",
+  ylab = "Chance of not seeing a different species (%)",
+  legend.title = "Species",
+  risk.table = FALSE,
+  censor = FALSE,
+  xlim = c(0, 500),
+  ggtheme = theme_minimal()
+)
 ################### SOARREMOARRE ###################
 # Setting up the dataset correctly for SM----
 SM_time2event_data <- observations_SM23_filtered |>
@@ -38,10 +95,60 @@ SM_time2event_data <- observations_SM23_filtered |>
     timegap = as.numeric(difftime(lead(eventStart), eventStart, units = "mins"))) |>
   dplyr::ungroup() |>
   dplyr::select(locationName, scientificName, next_species, timegap) |>
-  dplyr::filter(scientificName != next_species)
+  dplyr::filter(scientificName != next_species) |>
+  dplyr::mutate(event = 1,
+                timegap_hours = timegap / 60)
+
+#Plots SM----
+SM_time2event_data_summary <- SM_time2event_data %>%
+  group_by(focal_species = scientificName, recent_species = next_species) %>%
+  summarise(
+    mean_gap_hours = mean(timegap) ,          
+    sd = sd(timegap),
+    n = n(),
+    se = sd / sqrt(n),
+    lower_CI = mean_gap_hours - 1.96 * se,
+    upper_CI = mean_gap_hours + 1.96 * se,
+    .groups = "drop"
+  )
+
+ggplot(SM_time2event_data_summary, aes(x = recent_species, y = mean_gap_hours, color = recent_species)) +
+  geom_point(size = 3) +
+  geom_errorbar(aes(ymin = lower_CI, ymax = upper_CI), width = 0.2) +
+  facet_wrap(~ focal_species, scales = "free_y") +
+  labs(
+    title = "Effect of recent species detection on wait time Reitdiep",
+    x = "Previous detected species",
+    y = "Mean no. minutes between detections"
+  ) +
+  geom_text(aes(label = n), vjust = -1.5, size = 3.5, color = "black")+
+  theme_minimal(base_size = 14) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    legend.position = "none",
+    strip.background = element_rect(fill = "#CCCCCC"),
+    strip.text = element_text(face = "bold"))
+
+SM_surv <- Surv(time = SM_time2event_data$timegap_hours, event = SM_time2event_data$event)
+
+SM_km_fit <- survfit(SM_surv ~ scientificName, data = SM_time2event_data)
+
+ggsurvplot(
+  SM_km_fit,
+  data = SM_time2event_data,
+  conf.int = TRUE,
+  surv.scale = "percent",
+  xlab = "Time since species detected (hours)",
+  ylab = "Chance of not seeing a different species (%)",
+  legend.title = "Species",
+  risk.table = FALSE,
+  censor = FALSE,
+  xlim = c(0, 500),
+  ggtheme = theme_minimal()
+)
 
 ################### SOUTHWEST FRIESLAND ###################
-# Setting up the dataset correctly for SM----
+# Setting up the dataset correctly for SW----
 SW_time2event_data <- SW_data |>
   dplyr::filter(scientificName %in% Analysis_species) |>
   dplyr::mutate(scientificName =case_when(
@@ -56,6 +163,59 @@ SW_time2event_data <- SW_data |>
     timegap = as.numeric(difftime(lead(eventStart), eventStart, units = "mins"))) |>
   dplyr::ungroup() |>
   dplyr::select(locationName, scientificName, next_species, timegap) |>
-  dplyr::filter(scientificName != next_species)
+  dplyr::filter(scientificName != next_species) |>
+  dplyr::mutate(event = 1,
+                timegap_hours = timegap / 60)
+
+#Plots SW----
+SW_time2event_data_summary <- SW_time2event_data %>%
+  group_by(focal_species = scientificName, recent_species = next_species) %>%
+  summarise(
+    mean_gap_hours = mean(timegap) ,          
+    sd = sd(timegap),
+    n = n(),
+    se = sd / sqrt(n),
+    lower_CI = mean_gap_hours - 1.96 * se,
+    upper_CI = mean_gap_hours + 1.96 * se,
+    .groups = "drop"
+  )
+
+ggplot(SW_time2event_data_summary, aes(x = recent_species, y = mean_gap_hours, color = recent_species)) +
+  geom_point(size = 3) +
+  geom_errorbar(aes(ymin = lower_CI, ymax = upper_CI), width = 0.2) +
+  facet_wrap(~ focal_species, scales = "free_y") +
+  labs(
+    title = "Effect of recent species detection on wait time Zuidwest",
+    x = "Previous detected species",
+    y = "Mean no. minutes between detections"
+  ) +
+  geom_text(aes(label = n), vjust = -1.5, size = 3.5, color = "black")+
+  theme_minimal(base_size = 14) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    legend.position = "none",
+    strip.background = element_rect(fill = "#CCCCCC"),
+    strip.text = element_text(face = "bold")
+  )
+
+SW_time2event_data$facet_species <- SW_time2event_data$scientificName
+
+SW_surv <- Surv(time = SW_time2event_data$timegap_hours, event = SW_time2event_data$event)
+
+SW_km_fit <- survfit(Surv(timegap_hours, event) ~ next_species + facet_species, data = SW_time2event_data)
 
 
+ggsurvplot_facet(
+  fit = SW_km_fit,
+  data = SW_time2event_data,
+  facet.by = "scientificName",
+  conf.int = TRUE,
+  surv.scale = "percent",
+  xlab = "Time since species detected (hours)",
+  ylab = "Chance of not seeing another species (%)",
+  legend.title = "Species seen after",
+  censor = FALSE,
+  xlim = c(0, 200),
+  ggtheme = theme_minimal(),
+  palette = "Dark2"
+)
