b63008399e70cf51a69bea5ab7dcc6f1f83f2f25
Gijs van Male <g.van.male@student.rug.nl>
2025-06-05 16:11:42 +0200

    working on cox model

--- Scripts/7. Time_to_event_analysis.R
+++ Scripts/7. Time_to_event_analysis.R
@@ -8,6 +8,8 @@
   library(tidyverse)
   library(survival)
   library(survminer)
+  library(purrr)
+  library(broom)
   }
 
 ################### REITDIEPMIDDEN ###################
@@ -31,6 +33,16 @@ RM_time2event_data <- observations_RM23_filtered |>
   mutate(timegap_hours = timegap / 60) |>
   na.omit()
 
+# Filter for three different time intervals
+RM_time2event_60 <- RM_time2event_data |>
+  dplyr::filter(timegap_hours <= 1)
+
+RM_time2event_120 <- RM_time2event_data |>
+  dplyr::filter(timegap_hours <= 2)
+
+RM_time2event_24 <- RM_time2event_data |>
+  dplyr::filter(timegap_hours <= 24)
+
 #Plots RM----
 RM_time2event_data_summary <- RM_time2event_data %>%
   group_by(focal_species = scientificName, recent_species = next_species) %>%
@@ -42,7 +54,7 @@ RM_time2event_data_summary <- RM_time2event_data %>%
     lower_CI = mean_gap_hours - 1.96 * se,
     upper_CI = mean_gap_hours + 1.96 * se,
     .groups = "drop"
-  )
+  ) 
 
 ggplot(RM_time2event_data_summary, aes(x = recent_species, y = mean_gap_hours, color = recent_species)) +
   geom_point(size = 3) +
@@ -98,6 +110,16 @@ SM_time2event_data <- observations_SM23_filtered |>
   dplyr::filter(scientificName != next_species) |>
   dplyr::mutate(event = 1,
                 timegap_hours = timegap / 60)
+
+# Filter for three different time intervals
+SM_time2event_60 <- SM_time2event_data |>
+  dplyr::filter(timegap_hours <= 1)
+
+SM_time2event_120 <- SM_time2event_data |>
+  dplyr::filter(timegap_hours <= 2)
+
+SM_time2event_24 <- SM_time2event_data |>
+  dplyr::filter(timegap_hours <= 24)
 
 #Plots SM----
 SM_time2event_data_summary <- SM_time2event_data %>%
@@ -167,6 +189,16 @@ SW_time2event_data <- SW_data |>
   dplyr::mutate(event = 1,
                 timegap_hours = timegap / 60)
 
+# Filter for three different time intervals
+SW_time2event_60 <- SW_time2event_data |>
+  dplyr::filter(timegap_hours <= 1)
+
+SW_time2event_120 <- SW_time2event_data |>
+  dplyr::filter(timegap_hours <= 2)
+
+SW_time2event_24 <- SW_time2event_data |>
+  dplyr::filter(timegap_hours <= 24)
+
 #Plots SW----
 SW_time2event_data_summary <- SW_time2event_data %>%
   group_by(focal_species = scientificName, recent_species = next_species) %>%
@@ -219,3 +251,73 @@ ggsurvplot_facet(
   ggtheme = theme_minimal(),
   palette = "Dark2"
 )
+
+# Cox-Hazard -----
+# setting up a function
+fit_cox_and_plot_km <- function(df, dataset_name = "unknown", add_frailty = FALSE, return_plots = TRUE, plot_dir = NULL) {
+  unique_species <- unique(df$next_species)
+  
+  results <- list()
+  
+  for (target_species in unique_species) {
+    sub_df <- df %>% filter(next_species == target_species)
+    
+    
+    # Fit Cox model
+    formula <- if (add_frailty) {
+      as.formula("Surv(timegap_hours, event) ~ scientificName + frailty(locationName)")
+    } else {
+      as.formula("Surv(timegap_hours, event) ~ scientificName")
+    }
+    
+    model <- try(coxph(formula, data = sub_df), silent = TRUE)
+    if (inherits(model, "try-error")) next
+    
+    # Tidy model
+    model_summary <- tidy(model, conf.int = TRUE) %>%
+      mutate(next_species = target_species, dataset = dataset_name)
+    
+    # Kaplan-Meier plot
+    survfit_obj <- survfit(Surv(timegap_hours, event) ~ scientificName, data = sub_df)
+    plot_title <- paste("KM Curve:", dataset_name, "| Next:", target_species)
+    
+    km_plot <- ggsurvplot(
+      survfit_obj,
+      data = sub_df,
+      conf.int = TRUE,
+      pval = TRUE,
+      risk.table = TRUE,
+      title = plot_title,
+      ggtheme = theme_minimal()
+    )
+    
+
+    # Store results
+    results[[paste0(dataset_name, "_", target_species)]] <- list(
+      model = model_summary,
+      plot = if (return_plots) km_plot else NULL
+    )
+  }
+  
+  return(results)
+}
+
+# putting al the data in a list
+time2event_alldata <- list(
+  RM60 = RM_time2event_60,
+  RM120 = RM_time2event_120,
+  RM24 = RM_time2event_24,
+  SM60 = SM_time2event_60,
+  SM120 = SM_time2event_120,
+  SM24 = SM_time2event_24,  
+  SW60 = SW_time2event_60,
+  SW120 = SW_time2event_120,
+  SW24 = SW_time2event_24
+  )
+
+# run the function over all data
+time2event_results <- imap(time2event_alldata, ~ fit_cox_and_plot_km(.x, dataset_name = .y))
+
+print(time2event_results$SW24$`SW24_Felis catus`$plot)
+
+
